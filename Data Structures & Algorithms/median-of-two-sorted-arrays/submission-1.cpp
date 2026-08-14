9fa2e599ddf400e20d35b50de07c0254e3dba1ee
Pushpender <153177667+rpyaduvanshi950@users.noreply.github.com>
2026-08-14 09:20:16 +0530

    Update: median-of-two-sorted-arrays - submission-1

--- Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.cpp
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.cpp
@@ -1,29 +1,30 @@
-class TimeMap {
-private:
-    unordered_map<string, vector<pair<int, string>>> keyStore;
-
+class Solution {
 public:
-    TimeMap() {}
-
-    void set(string key, string value, int timestamp) {
-        keyStore[key].emplace_back(timestamp, value);
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int left = (nums1.size() + nums2.size() + 1) / 2;
+        int right = (nums1.size() + nums2.size() + 2) / 2;
+        return (getKth(nums1, nums1.size(), nums2, nums2.size(), left, 0, 0) +
+                getKth(nums1, nums1.size(), nums2, nums2.size(), right, 0, 0)) / 2.0;
     }
 
-    string get(string key, int timestamp) {
-        auto& values = keyStore[key];
-        int left = 0, right = values.size() - 1;
-        string result = "";
-
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (values[mid].first <= timestamp) {
-                result = values[mid].second;
-                left = mid + 1;
-            } else {
-                right = mid - 1;
-            }
+    int getKth(vector<int>& a, int m, vector<int>& b, int n, int k, int aStart, int bStart) {
+        if (m > n) {
+            return getKth(b, n, a, m, k, bStart, aStart);
+        }
+        if (m == 0) {
+            return b[bStart + k - 1];
+        }
+        if (k == 1) {
+            return min(a[aStart], b[bStart]);
         }
 
-        return result;
+        int i = min(m, k / 2);
+        int j = min(n, k / 2);
+
+        if (a[aStart + i - 1] > b[bStart + j - 1]) {
+            return getKth(a, m, b, n - j, k - j, aStart, bStart + j);
+        } else {
+            return getKth(a, m - i, b, n, k - i, aStart + i, bStart);
+        }
     }
 };

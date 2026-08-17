38191d6789696a6baffa1170b1db220705854acb
Pushpender <153177667+rpyaduvanshi950@users.noreply.github.com>
2026-08-17 15:17:30 +0530

    Add: subsets-ii - submission-0

---
+++ Data Structures & Algorithms/subsets-ii/submission-0.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res = {{}};
+        int prevIdx = 0;
+        int idx = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            idx = (i >= 1 && nums[i] == nums[i - 1]) ? prevIdx : 0;
+            prevIdx = res.size();
+            for (int j = idx; j < prevIdx; j++) {
+                std::vector<int> tmp = res[j];
+                tmp.push_back(nums[i]);
+                res.push_back(tmp);
+            }
+        }
+
+        return res;
+    }
+};
